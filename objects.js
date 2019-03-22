5c6dd4fd6659d5c6ede8206934a71c6d0bc94323
Aliya Lewis <aliyalewis5@gmail.com>
2019-03-22 16:46:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,2 @@
-var playlist = {artistName: "songTitle"};
+var playlist = {artistName: "songTitle"}
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
-}
